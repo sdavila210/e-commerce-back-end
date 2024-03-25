4d3b30891df11dec930eeb06f2968232f2934c3d
sarahd <sarahjdavila210@gmail.com>
2024-03-24 20:56:53 -0700

    updated index.js in models folder to create relationships

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,31 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  OnDelete: 'CASCADE'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  OnDelete: 'CASCADE'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+  OnDelete: 'CASCADE'
+})
+
 
 module.exports = {
   Product,
